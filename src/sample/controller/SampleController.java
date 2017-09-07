c0835dc43bbcfd3729cd92425ce1fb70d33ecc31
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-09-07 08:13:23 -0600

    Creating my process project and adding the gitignore file. I also added the controller and runner classes including the main method

---
+++ src/sample/controller/SampleController.java
@@ -0,0 +1,11 @@
+package sample.controller;
+
+public class SampleController 
+{
+	public void start()
+	{
+		System.out.println("This is a sample project to show the github");
+		System.out.println("and eclipse process and develop muscle memory");
+		
+	}
+}
