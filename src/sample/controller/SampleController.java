2d2b42af21327158bc3ae1ffcc6f1cddf2f49a42
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-09-07 09:50:40 -0600

    I added a scanner

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,4 +1,5 @@
 package sample.controller;
+import java.util.Scanner;
 
 public class SampleController 
 {
@@ -6,6 +7,15 @@ public class SampleController
 	{
 		System.out.println("This is a sample project to show the github");
 		System.out.println("and eclipse process and develop muscle memory");
-		
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		//Declares and substantiates scanner object
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your favorite food?");
+		String answer = input.nextLine();
+		System.out.println("Oh, you like to eat " + answer + "!");
 	}
 }
